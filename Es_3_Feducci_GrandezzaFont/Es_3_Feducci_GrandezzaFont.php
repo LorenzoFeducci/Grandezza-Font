8bc6c49a5055a0b30b4bf95998ccddfaa26faa03
LorenzoFeducci <lorefedu11@gmail.com>
2024-09-26 15:56:58 +0200

    grandezza font

---
+++ Es_3_Feducci_GrandezzaFont/Es_3_Feducci_GrandezzaFont.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    $grandezzaFont = rand(16, 60);
+
+    echo "<p style='font-size: $grandezzaFont" . "px'>ciao a tutti, $grandezzaFont px</p>"
+
+    ?>
+</body>
+</html>
